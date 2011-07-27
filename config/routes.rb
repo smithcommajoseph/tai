4e84a03ee864cc5fbf8196528d8ef3a47c1eb79b
Joseph Smith <technicolorenvy@gmail.com>
2011-07-27 17:14:29 -0600

    continually thinking things through... and flailing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Tai::Application.routes.draw do
-    
+  
   resource :twitter_account
   match '/callback/twitter/' => "twitter_accounts#callback", :as => :twitter_callback
   
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => "main#index"
 
 end
