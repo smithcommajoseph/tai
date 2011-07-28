8793612af99eed4c4a594ce8513472087fcfb273
Joseph Smith <technicolorenvy@gmail.com>
2011-07-28 01:33:57 -0600

    test for persistence

--- app/controllers/slight_controller.rb
+++ app/controllers/slight_controller.rb
@@ -5,6 +5,7 @@ class SlightController < ApplicationController
   end
   
   def whom
+    twitter_account = TwitterAccount.find_by_oauth_token(params[:oauth_token])
     twitter_account.post('@deanboyer, would ya look at it?');
     
   end
