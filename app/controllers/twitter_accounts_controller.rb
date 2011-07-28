46a1d83dd4cf9311e2228f5681abc2081eebc49c
Joseph Smith <technicolorenvy@gmail.com>
2011-07-28 09:20:02 -0600

    param pass test

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -13,7 +13,7 @@ class TwitterAccountsController < ApplicationController
       twitter_account.validate_oauth_token(params[:oauth_verifier], twitter_callback_url)
       twitter_account.save
       if twitter_account.active?
-        render(slight_generator_url, :notice => 'Twitter account activated!')
+        redirect_to(slight_generator_url, :notice => 'Twitter account activated!', :t => params[:oauth_token])
         
         # flash[:notice] = 'Twitter account activated!'
         # twitter_account.post('@deanboyer, would ya look at it?');
